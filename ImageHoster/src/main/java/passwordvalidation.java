e8beabdb0b4baefc11946b19ace78da0e9b32b9a
gauravdt” <elevenkg@gmail.com>
2019-04-07 13:52:19 +0530

    changed database password and port no to make them same as assignment stub project

--- ImageHoster/src/main/java/passwordvalidation.java
+++
@@ -1,7 +0,0 @@
-public class passwordvalidation {
-    public static void main(String[] args) {
-      String passwd = "a2@";
-      String pattern = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[@#$%^&+=]).{3,}";
-      System.out.println(passwd.matches(pattern));
-   }
-}
